4ce164881f2d6b5984d541e22843da0626ec8ad1
Narendra <159535924+narenmrkp@users.noreply.github.com>
2025-12-14 15:12:18 +0530

    Update query.py

--- server/diagnosis/query.py
+++ server/diagnosis/query.py
@@ -9,7 +9,7 @@ from langchain_groq import ChatGroq
 load_dotenv()
 
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
-PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "rbac-diagnosis-index")
+PINECONE_INDEX_NAME = os.getenv("PINECONE_INDEX_NAME", "medicaldiagnosis")
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
