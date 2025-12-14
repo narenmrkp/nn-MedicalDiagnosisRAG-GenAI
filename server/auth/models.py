293055513ad856997a85a6b4ff848ee77082c83b
Narendra <159535924+narenmrkp@users.noreply.github.com>
2025-12-14 16:33:07 +0530

    Update models.py

--- server/auth/models.py
+++ server/auth/models.py
@@ -1,9 +1,26 @@
-from pydantic import BaseModel
+# from pydantic import BaseModel
 
-class SignupRequest(BaseModel):
-    username:str
-    password:str
-    role:str 
+# class SignupRequest(BaseModel):
+#     username:str
+#     password:str
+#     role:str 
+# def verify_user(username: str, password: str):
+#     ...
+#     return user_or_none
+
+from server.config.db import users_collection
+import bcrypt
+
 def verify_user(username: str, password: str):
-    ...
-    return user_or_none
+    user = users_collection.find_one({"username": username})
+    if not user:
+        return None
+
+    if not bcrypt.checkpw(password.encode(), user["password"].encode()):
+        return None
+
+    return {
+        "username": user["username"],
+        "role": user["role"]
+    }
+
