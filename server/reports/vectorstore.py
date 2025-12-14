565e0957b359cc4038810cd470b7f6bef33340fb
Narendra <159535924+narenmrkp@users.noreply.github.com>
2025-12-14 15:01:48 +0530

    Update vectorstore.py

--- server/reports/vectorstore.py
+++ server/reports/vectorstore.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from tqdm.auto import tqdm
 from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from ..config.db import reports_collection
 from typing import List
